b0f6b712c4dd5ebe3b367d73e710b7305d89ca9b
mruona <mikko.ruonakoski@gmail.com>
2015-07-28 15:07:25 +0300

    New files

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello Worldi, joop')
+app.use(function(req, res, next)
+{
+    console.log(req.method);
+    next();
+});
+
+app.use('/', express.static('public'));
+
+app.use(bodyParser());
+
+app.post('/contact',function(req,res) {
+    console.log(req.body);
+    res.send("Your data is saved");
 });
 
 app.listen(3000);
